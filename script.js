3cc72b21a5fa18de254fd73a93fa0a3ba2eb01dd
Dteeech <marshall.isaac@protonmail.comgit configuser.email marshall.isaac@protonmail.com>
2022-12-19 14:42:54 +0000

    Ajout de la fonction de victoire

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    //recuperer les cases du tableau html
+    let morpion = ["", "", "", "", "", "", "", "", ""]
+    
     const winningPatterns = [
         [0, 1, 2],
         [3, 4, 5],
@@ -12,10 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
         [2, 4, 6]
     ]
 
+    //recuperer les cases du tableau html
 
-    let turn = 0;
+    
     const playerOne = '╳';
     const playerTwo = '◯';
+    let playerTurn = playerOne;
+    let gameStatus = document.getElementById("statutJeu")
+    let endGameStatus = gameStatus
     const case0 = document.getElementById("case0")
     const case1 = document.getElementById("case1")
     const case2 = document.getElementById("case2")
@@ -25,40 +30,49 @@ document.addEventListener("DOMContentLoaded", () => {
     const case6 = document.getElementById("case6")
     const case7 = document.getElementById("case7")
     const case8 = document.getElementById("case8")
-    const statutJeu = document.getElementById("statutJeu")
-    const bouttons = Array.from(document.getElementsByTagName("button"))
-
-    console.log(bouttons)
+    
+    const statutJeu = document.getElementById("statutJeu")//Affichage de la victoire
+    const bouttons = Array.from(document.getElementsByTagName("button"))// conversion de la node list en tableau
+    //On va comparer CETTE LISTE à notre Array WINNIN PATTERNS
+    
     //function cas de victoire
+    function isWinner() {
+    winningPatterns.forEach(casDeVictoire => {
+        const indexNb1 = casDeVictoire[0]
+        const indexNb2 = casDeVictoire[1]
+        const indexNb3 = casDeVictoire[2]
 
-    const victoryCases = () => {
-        //si 3 
+        console.log(morpion[indexNb1],morpion[indexNb2],morpion[indexNb3])
+        if (morpion[indexNb1] !== "" && morpion[indexNb2] !== "" && morpion[indexNb3] !== "") {
+            if (morpion[indexNb1] === morpion[indexNb2] && morpion[indexNb2] === morpion[indexNb3]) {
+                alert('Le gagnant est: ' + morpion[indexNb1]);
+            }
+        }
+
+    })
+}
+    //Fonction pour les tours
+    let playGame = (e) => {
+        e.target.innerHTML = playerTurn;
+        playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
     }
-
-    const allCases = (e) => {
-        if (turn === 0) {
-            e.innerText = playerOne
-            turn++
-        }
-        else if (turn >= 1) {
-            e.innerText = playerTwo
-            turn--
-        }
-
-    }   
+    
+    //parcourir les bouttons pour declencher le meme addeventlistener sur tous les bouttons
+    //On appelle la fonction qui permet d'ajouter une croix ou un rond
+    bouttons.forEach(function(item) {
+        item.addEventListener("click", playGame,{once: true})
+    })
+    
     // Function qui parcourt le tableau 
     //verifier que les cases ne sont pas vides
     winningPatterns.forEach(possibility => {
-        if (possibility[0] === possibility[1] && possibility[1] === possibility[2]) {
+        if (possibility[0] === possibility[1] && possibility[1] === possibility[2]) { //fonction de comparaison de cas de victoire avec le tableau des données
             alert('Vous avez gagné');
         }
     })
 
-    //ajout de la fonction allCases sur la boucle for each
-    //parcourir les bouttons pour declencher le meme addeventlistener sur tous les bouttons
-    bouttons.forEach(function(item) {
-        item.addEventListener("click", () => allCases(item))
-    })
+
+    
 
 
 
@@ -67,3 +81,31 @@ document.addEventListener("DOMContentLoaded", () => {
     // function jeu gagnant
     // function changer de joueur
 })
+
+////////////////PSEUDO CODE//////////////////////
+// but du morpion
+
+
+///////Depart//////
+
+
+
+//Commencer par le joueur 1 = croix
+//modifier le texte dans le boutton avec la croix, changer la valeur du boutton ?
+
+
+// ensuite joueur 2 = rond
+//modifier le texte dans le boutton avec le rond, changer la valeur du boutton ?
+
+
+
+//Creer une fonction qui voit si 3 elements sont collés et change la valeur de endGame en true : NOM : compareResultWithPatterns
+//endGame met fin à la partie et renvoi un message au vainqueur on l'appelle que en cas de victoire ou d'égalité
+
+
+
+
+// creer une fonction pour gerer les cas de victoire
+//Pour pouvoir comparer il faut comparer a un tableau avec touts les patterns de victoire possible
+// si 3 croix ou 3 ronds sont à la suite le joueur gagne
+// sinon egalité
