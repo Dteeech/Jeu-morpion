f211f7c37be145a3495e12bd0e01fae06a7533d0
Dteeech <marshall.isaac@protonmail.comgit configuser.email marshall.isaac@protonmail.com>
2022-12-19 09:02:07 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    //onclick
+    //["","",""] => notre jeu
+    //cas de victoire [[0,1,2], [3,4,5]]
+    // Btn
+    // function jeu gagnant
+    // function changer de joueur
+})
