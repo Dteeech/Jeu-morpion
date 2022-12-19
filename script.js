59a8072ea82e40fcc20ca26b25b1671b9ad915c4
Dteeech <marshall.isaac@protonmail.comgit configuser.email marshall.isaac@protonmail.com>
2022-12-19 09:38:33 +0000

    creation html css et devut de la logique du jeu en js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+    //recuperer les cases du tableau html
+    const case0 = document.getElementById("case0")
+    const case1 =document.getElementById("case1")
+    const case2 =document.getElementById("case2")
+    const case3 =document.getElementById("case3")
+    const case4 =document.getElementById("case4")
+    const case5 =document.getElementById("case5")
+    const case6 =document.getElementById("case6")
+    const case7 =document.getElementById("case7")
+    const case8 =document.getElementById("case8")
+    
+    //function cas de victoire
+    const victoryCases = () => {
+        
+    }
+    
+    
+    
     //onclick
+    const onClick = () => {
+        
+    }
     //["","",""] => notre jeu
     //cas de victoire [[0,1,2], [3,4,5]]
     // Btn
